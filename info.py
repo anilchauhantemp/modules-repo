befe219bb43837750e2081b0116f6897801aace1
Anil Chauhan <60502277+anilchauhanxda@users.noreply.github.com>
2020-03-12 16:57:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,8 +36,8 @@ def register(cb):
 @loader.tds
 class InfoMod(loader.Module):
     """Provides system information about the computer hosting this bot"""
-    strings = {"name": "System Info 👨🏻‍💻",
-               "info_title": "<b>System Info</b>",
+    strings = {"name": "System Info ",
+               "info_title": "<b>System Info 👨🏻‍💻</b>",
                "kernel": "<b>Kernel:</b> <code>{}</code>",
                "arch": "<b>Arch:</b> <code>{}</code>",
                "os": "<b>OS:</b> <code>{}</code>",
